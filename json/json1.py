67585175ce97642c829141f7fc97483694b19c71
Noah B <benoah2003@gmail.com>
2023-11-14 14:53:32 -0800

    json updated

--- json/json1.py
+++ json/json1.py
@@ -17,6 +17,7 @@ with open('data.json', 'w') as json_file:
     # Passing array data into json file, file indentation is 4 spaces
     json.dump(data,json_file,indent=4)
 
+# Confirming code works
 print('Data has been written to data.json')
 
 
@@ -26,6 +27,22 @@ with open('data.json', 'r') as json_file:
     # Create a loaded data object that loads the file
     loaded_data = json.load(json_file)
 
+# Confirming code works
 print('Successfully able to read data.json')
 # Prints data array from data json file
 print(loaded_data)
+
+
+# Change value of age in dictionary
+loaded_data['age'] = 34
+# Add item to end of list interests in dictionary
+loaded_data['interests'].append('nature')
+
+# Open json file and write in file
+with open('data.json', 'w') as json_file:
+
+    # Passing updated object into json file, file indentation is 4 spaces
+    json.dump(loaded_data, json_file, indent=4)
+
+# Confirming code works
+print('Modified data written to data.json')
